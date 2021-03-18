4a57cce264f05a168028fa80fbcfc06130e9dad5
omarm20 <57197677+omarm20@users.noreply.github.com>
2021-03-17 21:52:41 -0700

    Update registered.php

--- Rabbit_2/registered.php
+++ Rabbit_2/registered.php
@@ -5,57 +5,60 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 include ('functions.php');
+include "account.php";
+
+$db = mysqli_connect($dbIP, $dbLogin, $dbPass, $dbName);
 
 if (empty($_POST['name'])){
-    echo "Forgot to enter your Name";
+    echo "Forgot to enter your Name<br>";
     exit(header("Refresh: 2; url=register.html"));
 
-}
-if(empty($_POST['email'])){
-    echo "Forgot to enter your Email";
+}elseif (empty($_POST['email'])){
+    echo "Forgot to enter your Email<br>";
     exit(header("Refresh: 2; url=register.html"));
-}
-if(empty($_POST['password'])){
-    echo "Forgot to enter your Password";
+
+
+}elseif (email_exist($_POST['email'])){
+    echo "Email already exist try a different email<br>";
     exit(header("Refresh: 2; url=register.html"));
-}
-
-if (!check_pass_length($_POST['password'])){
-    echo 'Password must have 6 characters or more';
+}elseif (empty($_POST['password'])){
+    echo "Forgot to enter your Password<br>";
     exit(header("Refresh: 2; url=register.html"));
+
+}elseif (!check_pass_length($_POST['password'])){
+    echo "Password must have 6 characters or more<br>";
+    exit(header("Refresh: 2; url=register.html"));
+
+}else{
+
+    $passhash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $content = array(
+        "name" => $_POST['name'],
+        "email" => $_POST['email'],
+        "pass" => $passhash,
+        "type" => $_POST['submit']
+    );
+
+    $msgJson = json_encode($content);
+
+
+    $connection = new AMQPStreamConnection('25.14.30.215', 5672, 'admin', 'admin');
+    $channel = $connection->channel();
+    $channel->queue_declare('hello', false, false, false, false);
+
+    $msg = new AMQPMessage($msgJson, array('delivery_mode' =>2 ));
+
+    $channel->basic_publish($msg, '', 'hello');
+    
+    echo "Sent to RabbitMQ: ";
+    
+    header("Refresh: 4; url=login.php");
+
 }
-
-$passhash = password_hash($_POST['password'], PASSWORD_DEFAULT);
-
-$content = array(
-    "name" => $_POST['name'],
-    "email" => $_POST['email'],
-    "pass" => $passhash,
-    "type" => $_POST['submit']
-);
-
-$msgJson = json_encode($content);
-
-
-$connection = new AMQPStreamConnection('25.14.30.215', 5672, 'admin', 'admin');
-$channel = $connection->channel();
-$channel->queue_declare('hello', false, false, false, false);
-
-$msg = new AMQPMessage($msgJson, array('delivery_mode' =>2 ));
-
-$channel->basic_publish($msg, '', 'hello');
-
-header("location: login.php");
-
-echo "<br>";
-echo "Sent to RabbitMQ: ";
-echo $msg->body;
-echo "<br>";
 
 $channel->close();
 $connection->close();
 
-
-//echo "DB Login Confirmation: ";
-//echo $_SESSION['name'].", Your registration is complete!";
 ?>
+
